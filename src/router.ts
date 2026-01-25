57119029c67d99e53831169b925bc7905c0545c2
Anton <137686641+chikibambony137@users.noreply.github.com>
2026-01-25 23:03:17 +0300

    configure the project & init commit

---
+++ src/router.ts
@@ -0,0 +1,12 @@
+import { createMemoryHistory, createRouter } from 'vue-router'
+
+import App from './App.vue'
+
+const routes = [
+  { path: '/', component: App },
+]
+
+export const router = createRouter({
+  history: createMemoryHistory(),
+  routes,
+})
