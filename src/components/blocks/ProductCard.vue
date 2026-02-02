50d35817f503fe5f2f82569b70b94b1fb21bc30a
Anton <137686641+chikibambony137@users.noreply.github.com>
2026-02-02 22:11:54 +0300

    sketch product card

---
+++ src/components/blocks/ProductCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="grid gap-y-2 p-4 max-w-100 max-h-100 bg-white rounded-3xl">
+    <div class="flex justify-between">
+      <p>FIRMA</p>
+      <p>LIKE</p>
+    </div>
+
+    <div class="flex justify-center items-center">
+      <img class="max-h-70" src="@/assets/img/ear1.png" alt="ear1" />
+    </div>
+
+    <div class="flex justify-between">
+      <p>NAME</p>
+      <div class="grid gap-2 grid-cols-2 grid-rows-2">
+        <p class="flex justify-center col-span-2">PRICE</p>
+        <p>OLD PRICE</p>
+        <p>-32%</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
--- src/pages/Catalog.vue
+++ src/pages/Catalog.vue
@@ -11,12 +11,16 @@
       class="mx-6 mt-9"
       name="Чехлы"
       :cards="caseTypesStore.caseTypes"></HorizontalSwapper>
+
+    <ProductCard class="mx-6"></ProductCard>
   </main>
 </template>
 
 <script lang="ts" setup>
 import Banner from "../components/blocks/Banner.vue";
 import HorizontalSwapper from "../components/blocks/HorizontalSwapper.vue";
+import ProductCard from "../components/blocks/ProductCard.vue";
+
 import { useCaseTypesStore } from "../stores/useCaseTypesStore";
 
 const caseTypesStore = useCaseTypesStore();
